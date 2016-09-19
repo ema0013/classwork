528bf099beb08b3b697917386d463b760cc33964
Student 6 <Student 6@10.8.33.105>
2016-09-19 13:15:24 -0400

    new file DataType

---
+++ Introduction/src/datatypes/DataType.java
@@ -0,0 +1,18 @@
+package datatypes;
+
+public class DataType {
+
+	public DataType() {
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(5.0/2);//double divided by int
+		System.out.println((double)5/2);//casting
+		System.out.println(5/2);
+		System.out.println(3+5.0/2+5*2);
+		System.out.println(3.0+5/2+5*2);
+
+	}
+
+}
