ee0a49ee1536c6a0f5b26fa0d6ef1139991636e6
Student 6 <Student 6@10.8.33.105>
2016-09-26 12:31:16 -0400

    added findKeyWord method

--- Introduction/src/chatBot/EdwinMain.java
+++ Introduction/src/chatBot/EdwinMain.java
@@ -8,18 +8,18 @@ public class EdwinMain {
 	static boolean inLoop;
 	static String response;
 	static Topic school;
-	
+
 	public static void main(String[] args) {
 		createTopics();
 		promptName();
 		talkForever();
 	}
-	
+
 	public static void promptName() {
 		print("Hello human! I am a board covered with semi-conductors and other such electronic components.What is your name?");
 		user = input.nextLine();
 		print("Awesome! I will call you "+user+" until you terminate me.");
-		
+
 	}
 
 	public static void talkForever(){
@@ -27,7 +27,7 @@ public class EdwinMain {
 		while (inLoop){
 			print("Greetings "+user+". How are you?");
 			response = getInput();
-			if(response.indexOf("good")>=0){
+			if(findKeyWord(response,"good",0)){
 				print("I'm so happy you're good.");
 			}
 			else if (response.indexOf("school")>= 0){
@@ -39,13 +39,43 @@ public class EdwinMain {
 		}
 	}
 
+	public static boolean findKeyWord(String searchString, String key, int startIndex) {
+		//delete white space
+		String phrase = searchString.trim();
+		//set all letters to lower case
+		phrase = phrase.toLowerCase();
+		key = key.toLowerCase();
+		//find position of key
+		int psn = phrase.indexOf(key);
+		//keep looking for word until you find the right context
+		while(psn >=0 ){
+			String before = " ";
+			String after = " ";
+			// if phrase doesn't end with this word
+			if(psn + key.length() < phrase.length()){
+				after = phrase.substring(psn + key.length(), psn + key.length() + 1).toLowerCase();
+			}
+			//if the phrase doesn't begin with this word
+			if(psn > 0){
+				before = phrase.substring(psn-1,psn).toLowerCase();
+			}
+			if(before.compareTo("a") < 0 && after.compareTo("a") < 0){
+				return true;
+			}
+			// in case keyword wasn't found yet, repeat and check the rest of string
+			psn = phrase.indexOf(key,psn+1);
+		}
+
+		return false;
+	}
+
 	public static void promptInput() {
 		print("Try inputting a string!");
 		String userInput = input.nextLine();
 		print("You typed: "+userInput);
 
 	}
-	
+
 	public static String getInput(){
 		return input.nextLine();
 	}
@@ -74,7 +104,7 @@ public class EdwinMain {
 			}
 			printString += currentLine + "\n";
 		}
-		
+
 		System.out.println(printString);
 
 	}
@@ -82,7 +112,8 @@ public class EdwinMain {
 	public static void createTopics() {
 		input = new Scanner(System.in);
 		school = new School();
+
 	}
-	
+
 
 }
