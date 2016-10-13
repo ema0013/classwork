19e28c8ef6cea90a26458f9fcb2d34aa52ec2bea
Student 6 <Student 6@10.8.33.105>
2016-10-13 13:15:47 -0400

    did stuff

--- Introduction/src/arrays/ArrayMain.java
+++ Introduction/src/arrays/ArrayMain.java
@@ -5,9 +5,29 @@ public class ArrayMain {
 	public static void main(String[] args) {
 		//this is how you time how quickly a computer processes
 		long startTime = System.currentTimeMillis();
-		arrayIntroMethod();
+		String [] someStrings = new String[100];
+		populateArray(someStrings);
+		changeString(someStrings[99]);
+		printArray(someStrings);
 		long endTime = System.currentTimeMillis();
 		System.out.println("Completed method in " +(endTime-startTime)+ " milliseconds");
+		arrayDemonstrations();
+	}
+	
+	private static void printArray(String[] a) {
+		for(String s: a){
+			System.out.println(s);
+		}
+	}
+	
+	private static void populateArray(String[] a) {
+		for(int index = 0; index < a.length; index++){
+			a[index] = "value "+(index+1);
+		}
+	}
+	
+	private static void changeString(String s){
+		s = "This string has been changed!";
 	}
 	public static void arrayIntroMethod(){
 		//construct two integer arrays
@@ -42,10 +62,25 @@ public class ArrayMain {
 		for(String s: strings1){
 			System.out.println(s);
 		}
-
+		
+		for(int index = 0; index < strings2.length; index++){
+			strings2[index] = "value "+(index+1);
+		}
 		for(String s: strings2){
 			System.out.println(s);
 
+		}
+	}
+	public static void arrayDemonstrations(){
+		int []numbers = new int[50];
+		for (int i = 0; i < numbers.length; i++){
+			numbers[i] = i+1;
+			System.out.println(numbers[i]);
+		}
+		int []numbers2 = new int[(int)(Math.random()*100)+100];
+		for (int i = 0; i < numbers2.length; i++){
+			numbers[i] = (int)(Math.random()*numbers2.length)+(int)(Math.random()*10)+10;
+			System.out.println(numbers[i]);
 		}
 	}
 }
