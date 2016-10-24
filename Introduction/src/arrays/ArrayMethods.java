3d38b895f678e2b25b6604ed574852f3a07ac8c5
Student 6 <Student 6@10.8.33.105>
2016-10-24 12:29:46 -0400

    more stuff

--- Introduction/src/arrays/ArrayMethods.java
+++ Introduction/src/arrays/ArrayMethods.java
@@ -5,7 +5,8 @@ public class ArrayMethods {
 	public static void main(String[] args) {
 		int[] arr = {2,3,4,6,9,11,12,15};
 //		swap(arr,0,arr.length-1);d
-		shuffle(arr);
+		print(arr);
+		cycleThrough(arr,1);
 		print(arr);
 //		if(checkHalfWay(arr,12,0,arr.length-1)){
 //			System.out.println("The number you are searching for is less than"
@@ -15,6 +16,22 @@ public class ArrayMethods {
 //			System.out.println("The number you are searching for is greater than "
 //					+ "the value in the middle of the array");
 //		}
+	}
+	public static void cycleThrough(int[] array, int n){
+		for(int i = 0; i < n; i ++){
+			cycleOnce(array);
+		}
+	}
+	private static void cycleOnce(int[] array){
+		for(int j = 0; j<array.length-1;j++){
+			if(j == 0){
+				swap(array,array.length-1,j);
+			}
+			if(j == array.length-2){
+				return;
+			}
+				swap(array,j,j+1);
+		}
 	}
 	/**
 	 * returns true if searchValue is less than element 
