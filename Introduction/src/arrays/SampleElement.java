4ec011d02966fe24cb9b425a3949da84713b3fe7
Student 6 <Student 6@10.8.33.105>
2016-10-27 12:56:54 -0400

    did stuff

--- Introduction/src/arrays/SampleElement.java
+++ Introduction/src/arrays/SampleElement.java
@@ -17,4 +17,33 @@ public class SampleElement {
 		//x++;
 		number++;
 	}
+	
+	
+	public static int longestSharedSequence(int[] array1, int[] array2){
+		int count = 1;
+		int longestSequence = 1;
+		for(int i=0;i<array1.length;i++){
+			for(int j =0;j<array2.length;j++){
+				boolean inLoop  = true;
+				int inc = 1;
+				if(array1[i]==array2[j]){
+					while(inLoop){
+						if(inc+i<array1.length && inc+j<array2.length && array1[inc+i]==array2[inc+j]){
+							inc++;
+							count++;
+						}
+						else{
+							inLoop = false;
+							if(count>longestSequence){
+								longestSequence = count;
+								count = 0;
+							}
+						}
+					}
+				}
+			}
+			
+		}
+		return longestSequence;
+	}
 }
