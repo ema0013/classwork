263ef2d9b74489e441f3b3eab8bbf42b1a787d63
Student 6 <Student 6@10.8.33.105>
2016-11-01 12:02:46 -0400

    did stuff

--- Introduction/src/caveExplorer/CaveRoom.java
+++ Introduction/src/caveExplorer/CaveRoom.java
@@ -2,4 +2,29 @@ package caveExplorer;
 
 public class CaveRoom {
 
+	public CaveRoom(String description) {
+		// TODO Auto-generated constructor stub
+	}
+	//"final" never changes
+	//constants are public and ALL-CAPS
+	//example Math.PI;
+	public static final int NORTH = 0;
+	public static final int EAST = 1;
+	public static final int SOUTH = 2;
+	public static final int WEST = 3;
+	
+	private String Description;
+	private String directions;
+	private String contents;
+	private String defaultContents;
+	
+	private CaveRoom[] borderingRooms;
+	private Door[] doors;
+	
+	
+	public void enter() {
+		// TODO Auto-generated method stub
+		//sets up cave when it gets entered
+	}
+	 
 }
---
+++ Introduction/src/caveExplorer/Door.java
@@ -0,0 +1,8 @@
+package caveExplorer;
+
+public class Door {
+	
+	private boolean locked;
+	private boolean open;
+	
+}
