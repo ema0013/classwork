1da4f5174ebf1d7d231dce9f423c8d1c5833e3dd
Student 6 <Student 6@10.8.33.105>
2016-12-14 13:17:55 -0500

    L

--- Introduction/src/gui/components/Button.java
+++ Introduction/src/gui/components/Button.java
@@ -18,14 +18,24 @@ public class Button extends TextLabel implements Clickable {
 		update();
 	}
 	
+	public Color getColor(){
+		return this.color;
+	}
+	
+	public void setColor(Color c){
+		this.color = c;
+		update();
+	}
+	
 	public void update(Graphics2D g) {
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		g.setColor(color);
-		g.drawRoundRect(getX(),getY(),getWidth(),getHeight(), 28, 28);
-		g.fillRoundRect(getX(),getY(), getWidth(), getHeight(), 28, 28);
-		g.setFont(new Font(getFont(),Font.PLAIN,getSize()));
+		g.fillRoundRect(0,0, getWidth(),getHeight(), 28, 28);
 		g.setColor(Color.black);
+		g.drawRoundRect(1,1,getWidth()-2,getHeight()-2, 28, 28);
+		g.setFont(new Font(getFont(),Font.PLAIN,getSize()));
 		if(getText()!= null){
+			g.setColor(Color.white);
 			g.drawString(getText(),4,getHeight()-5);
 		}
 
@@ -33,12 +43,7 @@ public class Button extends TextLabel implements Clickable {
 
 	@Override
 	public boolean isHovered(int x, int y) {
-		if(getX()+getWidth()-x<0)
-			return false;
-		if(getY()+getHeight()-y<0){
-			return false;
-		}
-		return true;
+		return x>getX() && x<getX()+getWidth() && y > getY() && y<getY()+getHeight();
 	}
 
 	@Override
