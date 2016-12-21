2b187d41ebc482a7bcc66812cc2356e73a7b128b
Student 6 <Student 6@10.8.33.105>
2016-12-21 12:34:14 -0500

    asdasdasdasdasd

--- Introduction/src/gui6/sampleGames/SadReactsOnly.java
+++ Introduction/src/gui6/sampleGames/SadReactsOnly.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 
 import gui6.Screen;
 import gui6.components.Graphic;
+import gui6.components.TextArea;
 import gui6.components.Visible;
 
 public class SadReactsOnly extends Screen {
 
 	private Graphic sadreact; 
+	private TextArea text;
 	
 	public SadReactsOnly(int width, int height) {
 		super(width, height);
@@ -19,6 +21,8 @@ public class SadReactsOnly extends Screen {
 	public void initObjects(ArrayList<Visible> viewObjects) {
 		sadreact = new Graphic(100,100,"resources/sampleImages/sadreactsonly.png");
 		viewObjects.add(sadreact);
+		text = new TextArea(20,300,50,50,"SAD REACTS ONLY");
+		viewObjects.add(text);
 
 	}
 
